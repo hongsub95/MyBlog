bdd6ca4c753091ce2b07fc834e5c84b85bde5a3b
hongsub <hong9506@gmail.com>
2022-11-16 19:29:11 +0900

    Board models, admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Board)
+class BoardAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("기본정보", {
+            "fields": ("title","content","tag","writer",),
+        }),
+    )
+    
+    list_display = ("title","writer","Tag","created_at")
+    search_fields = ("title","Tag")
+    
+    def Tag(self,obj):
+        tags = obj.tag.all()
+        tag_list = []
+        for t in tags:
+            tag_list.append(t)
+        return tag_list if len(tag_list) < 5 else tag_list[:5]
+    Tag.short_description = "태그"
+    
+    
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("기본정보", {
+            "fields": (
+                "name",
+            ),
+        }),
+    )
+    list_display = ("name",)
+    
--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=50,verbose_name="제목")
+    content = models.TextField(verbose_name="내용")
+    tag = models.ManyToManyField("Tag",blank=True,verbose_name="태그")
+    writer = models.ForeignKey("users.User",related_name="boards",verbose_name="작성자",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성날짜")  
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정날짜")  
+    class Meta:
+        verbose_name_plural = "게시판"
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30,verbose_name="태그이름")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성날짜")  
+    class Meta:
+        verbose_name_plural = "태그"
+    
+    def __str__(self):
+        return self.name
